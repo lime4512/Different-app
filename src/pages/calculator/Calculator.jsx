0aae716d621568233f011cb22773d99477243086
Alex Kruglov <lime4512@mail.ru>
2023-11-02 13:42:48 +0300

    add new input

--- src/pages/calculator/Calculator.jsx
+++ src/pages/calculator/Calculator.jsx
@@ -2,6 +2,10 @@ const Calculator = () => {
 	return (
 		<div className='main-calculator'>
 			<h1>Калькулятор</h1>
+			<input></input>
+			<input></input>
+			<input></input>
+			<button></button>
 		</div>
 	)
 }
